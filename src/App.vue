337aefe5f801dea4189618ccce95b11c89b58c45
Satoru Nakamura <na.kamura.1263@gmail.com>
2019-07-14 12:23:51 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-app>
+        <v-toolbar>
+            <v-toolbar-title class="headline">
+                カレンダー検索
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items>
+                <v-btn flat>
+                    <router-link to="/"><i class="material-icons">home</i></router-link>
+                </v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+    
+        <v-content>
+            <router-view/>
+        </v-content>
+    
+        <v-footer dark height="auto">
+            <v-card class="flex" flat tile>
+    
+                <v-card-actions class="grey darken-3 justify-center my-5 text-xs-center">
+                    Satoru Nakamura
+                </v-card-actions>
+            </v-card>
+        </v-footer>
+    </v-app>
+</template>
